75fae922509824180c594e54ec3d0ea24bc63bef
jhpeng <jp549@duke.edu>
2023-07-18 13:55:21 -0400

    qusistation state implementation

--- ct_sis_model/main.c
+++ ct_sis_model/main.c
@@ -33,21 +33,133 @@ static double mean(double* sequence, int length) {
     return mean_value/length;
 }
 
-/**
- * @brief Performs quasistationary state sampling for an epidemic process.
- *
- * This function performs quasistationary state sampling for an epidemic process, calculating the infected ratio,
- * infected ratio squared, and number of trials. It stores these values in separate output files.
- *
- * @param edges Pointer to an array of edge pairs representing the network connections.
- * @param nnode The total number of nodes in the network.
- * @param nedge The total number of edges in the network.
- * @param alpha The alpha parameter for the epidemic model.
- * @param gamma The gamma parameter for the epidemic model.
- * @param t The time threshold for the sampling process.
- * @param nsample The number of samples to be generated.
- * @param rng Pointer to a GSL random number generator.
- */
+void qs_sampling_memory(const int* edges, int nnode, int nedge, double alpha, double gamma, int nsample, gsl_rng* rng) {
+    int* sigma = (int*)malloc(sizeof(int)*nnode);
+    int* propose = (int*)malloc(sizeof(int)*nnode);
+    int* distrib = (int*)malloc(sizeof(int)*nnode);
+
+    for(int i=0;i<nnode;i++) {
+        if(gsl_rng_uniform_pos(rng)<0.5) {
+            sigma[i] = 1;
+        } else {
+            sigma[i] = 0;
+        }
+        distrib[i] = 0;
+    }
+
+    int M = 100;
+    int* sigma_history = (int*)malloc(sizeof(int)*nnode*M);
+
+    for(int i=0;i<(nnode*M);i++) {
+        if(gsl_rng_uniform_pos(rng)<0.5) {
+            sigma_history[i] = 1;
+        } else {
+            sigma_history[i] = 0;
+        }
+    }
+
+    clock_t start, end;
+    double cpu_time_used;
+
+    start = clock();
+    printf("start of thermalization...\n");
+
+    double pr = 0.01;
+    double dt = 0.0;
+    int np=0;
+    int nreject=0;
+    int nthermal = M*1000;
+    for(int i_thermal=0; i_thermal<nthermal;) {
+        dt = 0;
+        while(dt*pr<gsl_rng_uniform_pos(rng)) {
+            np = kernel_uniform_scan(alpha, gamma, &dt, nnode, nedge, sigma, edges, propose, rng);
+            kernel_update(np, propose, sigma);
+        }
+
+        int infected_number=0;
+        for(int i=0; i<nnode; i++) {
+            infected_number += sigma[i];
+        }
+
+        if(infected_number==0) {
+            int ihist = (int)(gsl_rng_uniform_pos(rng)*M);
+            for(int i=0;i<nnode;i++) {
+                sigma[i] = sigma_history[nnode*ihist+i];
+            }
+            nreject++;
+        } else {
+            int ihist = i_thermal%M;
+            for(int i=0;i<nnode;i++) {
+                sigma_history[nnode*ihist+i] = sigma[i];
+            }
+
+            if((i_thermal+1)%100==0) {
+                double percentage = (double)(i_thermal+1)*100/nthermal;
+                printf("%lf ( %d %d) \n",percentage, nreject, infected_number);
+            }
+            i_thermal++;
+            nreject=0;
+        }
+    }
+
+    end = clock();
+    cpu_time_used = (double)(end-start)/CLOCKS_PER_SEC;
+    printf("end of thermalization...\n");
+    printf("cpu time used : %lf s\n", cpu_time_used);
+    start = clock();
+
+    pr = 0.01;
+    printf("start sampling...\n");
+    nreject=0;
+    for(int i_sample=0; i_sample<nsample;) {
+        dt = 0;
+        while(dt*pr<gsl_rng_uniform_pos(rng)) {
+            np = kernel_uniform_scan(alpha, gamma, &dt, nnode, nedge, sigma, edges, propose, rng);
+            kernel_update(np, propose, sigma);
+        }
+
+        int infected_number=0;
+        for(int i=0; i<nnode; i++) {
+            infected_number += sigma[i];
+        }
+
+        if(infected_number==0) {
+            int ihist = (int)(gsl_rng_uniform_pos(rng)*M);
+            for(int i=0;i<nnode;i++) {
+                sigma[i] = sigma_history[nnode*ihist+i];
+            }
+            nreject++;
+        } else {
+            int ihist = (int)(gsl_rng_uniform_pos(rng)*M);
+            for(int i=0;i<nnode;i++) {
+                sigma_history[nnode*ihist+i] = sigma[i];
+                distrib[i] += sigma[i];
+            }
+
+            end = clock();
+            cpu_time_used = (double)(end-start)/CLOCKS_PER_SEC;
+
+            FILE* sampling_file = fopen("sampling_data.txt","a");
+            printf("%d %d %d %lf \n", i_sample, nreject, infected_number, cpu_time_used);
+            fprintf(sampling_file, "%d %d %lf \n", nreject, infected_number, cpu_time_used);
+
+            fclose(sampling_file);
+
+            start = clock();
+
+            i_sample++;
+            nreject=0;
+        }
+    }
+
+    FILE* distrib_file = fopen("distrib_data.txt","w");
+    for(int i=0;i<nnode;i++) {
+        fprintf(distrib_file,"%d ",distrib[i]);
+    }
+    fprintf(distrib_file,"\n");
+    fclose(distrib_file);
+    
+}
 
 void qs_sampling_largeTlimit(const int* edges, int nnode, int nedge, double alpha, double gamma, double ir, double t, int nsample, gsl_rng* rng) {
     int* sigma   = (int*)malloc(sizeof(int)*nnode);
@@ -115,6 +227,7 @@ void qs_sampling_largeTlimit(const int* edges, int nnode, int nedge, double alph
 
 int main(int argc, char* argv[]) {
     int N = 100;
+    int mode = 1;
     double rho = 1.0;
     double alpha = 1.0;
     double gamma = 1.0;
@@ -125,8 +238,11 @@ int main(int argc, char* argv[]) {
 
 
     int opt;
-    while ((opt = getopt(argc, argv, "N:r:a:g:i:T:n:s:")) != -1) { // Fixed option string
+    while ((opt = getopt(argc, argv, "m:N:r:a:g:i:T:n:s:")) != -1) { // Fixed option string
         switch (opt) {
+            case 'm':
+                mode = atoi(optarg);
+                break;
             case 'N':
                 N = atoi(optarg);
                 break;
@@ -166,13 +282,17 @@ int main(int argc, char* argv[]) {
     int* edges = kinship_graphs_generator(&nnode, &nedge, N, rho, rng);
     double beta = ((double)(nedge*2))/nnode;
 
-    qs_sampling_largeTlimit(edges, nnode, nedge, alpha, gamma, initial_ratio, T, nsample, rng) ;
+    if(mode) {
+        qs_sampling_largeTlimit(edges, nnode, nedge, alpha, gamma, initial_ratio, T, nsample, rng) ;
+    } else {
+        qs_sampling_memory(edges, nnode, nedge, alpha, gamma, nsample, rng);
+    }
 
     FILE* beta_file = (FILE*)fopen("ave_number_edge_per_node.dat","a");
     fprintf(beta_file, "%.12f \n", beta);
     fclose(beta_file);
 
-    FILE* edge_file = (FILE*)fopen("edges.dat","a");
+    FILE* edge_file = (FILE*)fopen("edges.dat","w");
     for(int i=0; i<nedge; i++) {
         fprintf(edge_file, "%d %d \n", edges[i*2],edges[i*2+1]);
     }
